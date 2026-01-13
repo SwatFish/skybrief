98b1e346940963a879ac4ae89e59ec09cbb9913f
SwatFish <andres_belgy@msn.com>
2026-01-13 21:52:25 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "aviator-s-compass",
+  base: "/aviator-s-compass/",
 
   server: {
     host: "::",
